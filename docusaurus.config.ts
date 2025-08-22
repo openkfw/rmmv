73a06be31c6f55f362e0c6ba925a402e4c211521
S-r-f-l <wsurafel@ymail.com>
2025-08-22 12:11:16 +0200

    Add SVG illustration for "Focus on What Matters" to static images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/openkfw/rmmv/",
+            "https://openkfw.github.io/rmmv/",
         },
         theme: {
           customCss: "./src/css/custom.css",
