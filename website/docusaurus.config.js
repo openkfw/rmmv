3ee245cd33665c06f705d9868d8c434a91ca4b19
S-r-f-l <wsurafel@ymail.com>
2025-09-04 12:52:47 +0200

    images update to the site

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,38 +4,27 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-const prismThemes = require('prism-react-renderer').themes;
+const {themes} = require('prism-react-renderer');
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  trailingSlash: false, // or true, depending on your preference
   title: 'RMMV Guidebook',
   tagline: 'Remote Management, Monitoring, & Verification (RMMV) Guidebook',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
   // Set the production url of your site here
   url: 'https://openkfw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rmmv/', // Should match your repo name
+  baseUrl: '/rmmv/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  // GitHub pages deployment config
   organizationName: 'openkfw',
-  projectName: 'rmmv', // Should match your repo name
+  projectName: 'rmmv',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -47,30 +36,13 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/openkfw/rmmv/tree/main/website/',
+          editUrl: 'https://github.com/openkfw/rmmv/tree/main/website/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/openkfw/rmmv/tree/main/website/blog/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false, // Disable blog completely
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -79,22 +51,15 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/Koffer_3_Responsive_1080x608.png',
       navbar: {
         title: 'RMMV Guidebook',
         logo: {
-          alt: 'rmmv Logo',
-          src: 'C:\\Users\\rmmv\\website\\static\\img\\Koffer_3_Responsive_1080x608.png',
+          alt: 'KfW RMMV Logo',
+          src: 'img/Koffer_3_Responsive_1080x608.png',
+          height: 32,
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/openkfw/rmmv',
             label: 'GitHub',
@@ -106,50 +71,20 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
+            title: 'Resources',
             items: [
-              {
-                label: 'Executive Summary',
-                to: '/executive_summary',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/openkfw/rmmv',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} KfW Development Bank.`,
       },
       prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        theme: themes.github,
+        darkTheme: themes.dracula,
       },
     }),
 };
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,53 +1,24 @@
-// @ts-check
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 /**
  * Creating a sidebar enables you to:
  - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
+ - render a "Next" navigation link in the doc footer
+ - provide a table of contents on the right side of your page
 
  The sidebars can be generated from the filesystem, or explicitly defined here.
 
  Create as many sidebars as you want.
-
- @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
  */
+
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  // But you can create a sidebar manually
+  // By default, Docusaurus generates a sidebar from the docs folder structure
   tutorialSidebar: [
-    'intro',
-    {
-      type: 'category',
-      label: 'RMMV',
-      items: [
-        'executive_summary',
-        'disclaimer',
-        'introduction',
-      ],
-    },
-    { 
-      type: 'category',
-      label: 'Tutorial Basics',
-      items: [
-        'tutorial-basics/congratulations',
-        'tutorial-basics/create-a-blog-post',
-        'tutorial-basics/create-a-document',
-        'tutorial-basics/create-a-page',
-        'tutorial-basics/deploy-your-site',
-        'tutorial-basics/markdown-features',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Tutorial Extras',
-      items: [
-        'tutorial-extras/manage-docs-versions',
-        'tutorial-extras/translate-your-site',
-      ],
-    },
+    'introduction',
+    'executive_summary',
+    'disclaimer',
   ],
 };
 
-export default sidebars;
+module.exports = sidebars;
