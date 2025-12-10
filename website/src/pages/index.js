d723506e27dc5d6158d413d86ef38ccfd434de22
Surafel <wsurafel@ymail.com>
2025-12-10 16:53:08 +0100

    Updated homepage “Get Started” button

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="/introduction">
+              to="/table-of-contents">
               Get Started
             </Link>
           </div>
